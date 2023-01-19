f6fef44beffcc3aecf1d121a5cc24e79785742e7
anusha <anusha@LAPTOP-INCQFG99>
2023-01-19 10:30:58 +0530

    First commit

---
+++ src/test/java/practice_maven/CreateTest.java
@@ -0,0 +1,20 @@
+package practice_maven;
+
+import org.testng.annotations.Test;
+
+public class CreateTest 
+{
+@Test
+	public void create()
+	{
+		System.out.println("test created");
+	}
+@Test
+	public void update()
+	{
+		System.out.print("test updated");
+	}
+
+	
+
+}
